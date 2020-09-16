2c6d20b0ffbe4ce185d0c99548bf5c7f6c1ed965
Jerry Goodnough <jerry.goodnough@stormwoods.com>
2020-09-16 15:03:16 -0700

    Find Practitioner Organization

--- src/main/java/com/cognitive/nih/niddk/mccapi/mappers/PractitionerMapper.java
+++ src/main/java/com/cognitive/nih/niddk/mccapi/mappers/PractitionerMapper.java
@@ -2,14 +2,14 @@ package com.cognitive.nih.niddk.mccapi.mappers;
 
 import com.cognitive.nih.niddk.mccapi.data.Contact;
 import com.cognitive.nih.niddk.mccapi.data.Context;
+import com.cognitive.nih.niddk.mccapi.services.ReferenceResolver;
 import com.cognitive.nih.niddk.mccapi.util.Helper;
-import org.hl7.fhir.r4.model.Address;
-import org.hl7.fhir.r4.model.ContactPoint;
-import org.hl7.fhir.r4.model.Practitioner;
+import lombok.extern.slf4j.Slf4j;
+import org.hl7.fhir.r4.model.*;
 
 import java.util.List;
 import java.util.Map;
-
+@Slf4j
 public class PractitionerMapper {
     public static Contact fhir2Contact(Practitioner in, Context ctx) {
         Contact out = new Contact();
@@ -33,8 +33,46 @@ public class PractitionerMapper {
         if (bestEmail != null) {
             out.setEmail(bestEmail.getValue());
         }
+        try {
+            String id = in.getId();
+            Bundle bundle = ctx.getClient().search().forResource(PractitionerRole.class).where(PractitionerRole.PRACTITIONER.hasId(id)).returnBundle(Bundle.class).execute();
+            StringBuffer orgs = new StringBuffer();
+            int orgCount = 0;
 
+            for (Bundle.BundleEntryComponent e : bundle.getEntry()) {
+                if (e.getResource().fhirType() == "PractitionerRole") {
+                    PractitionerRole pr = (PractitionerRole) e.getResource();
+                    boolean isOk = true;
+                    if (pr.hasPeriod())
+                    {
+                         isOk =Helper.isInPeriod(pr.getPeriod(),ctx.getNow());
+                    }
 
+                    if (isOk)
+                    {
+                        Reference ref = pr.getOrganization();
+                        if (ref.hasDisplay())
+                        {
+                            Helper.addStringToBufferWithSep(orgs,ref.getDisplay(),", ");
+                        }
+                        else
+                        {
+                            Organization o = ReferenceResolver.findOrganization(ref,ctx);
+                            if (o != null)
+                            {
+                                Helper.addStringToBufferWithSep(orgs,o.getName(),", ");
+                            }
+                        }
+                    }
+                }
+            }
+            out.setOrganizationName(orgs.toString());
+
+        }
+        catch(Exception e)
+        {
+            log.warn("Error fetching organization name",e);
+        }
         out.setType(Contact.TYPE_PERSON);
         out.setRole(Contact.ROLE_PROVIDER);
         return out;
