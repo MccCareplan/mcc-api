0c8eec477c0bd2c6dd8335d742fcac87bc597b13
Jerry Goodnough <jerry.goodnough@stormwoods.com>
2020-09-25 16:40:53 -0700

    Revert to old Manager

--- src/main/java/com/cognitive/nih/niddk/mccapi/managers/ProfileManager.java
+++ src/main/java/com/cognitive/nih/niddk/mccapi/managers/ProfileManager.java
@@ -1,49 +1,22 @@
 package com.cognitive.nih.niddk.mccapi.managers;
 
 import org.hl7.fhir.r4.model.CodeableConcept;
-import org.hl7.fhir.r4.model.Coding;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 
 public class ProfileManager {
     private static ProfileManager singlton = new ProfileManager();
 
     public static ProfileManager getProfileManager() {return singlton;}
 
-    private static HashMap<String, String> profileMap = new HashMap<>();
-
-    static {
-        profileMap.put("2.16.840.1.113762.1.4.1222.159","CKD");
-    }
-    public String getProfilesForConcept(CodeableConcept concept)
+    public String getProfileForConcept(CodeableConcept concept)
     {
-        //Look for more than one coding.
-        StringBuilder out = new StringBuilder();
+        String out = null;
+        //TODO: Value Set Test against each profile.
+        //      First match wins
 
-        List<Coding> cds = concept.getCoding();
-        for (Coding cd: cds) {
-            ArrayList<String> match = ValueSetManager.getValueSetManager().findCodesValuesSets(cd.getSystem(), cd.getCode());
-
-            if (match != null) {
-
-                for (String vs : match) {
-                    if (profileMap.containsKey(vs)) {
-                        if (out.length() > 0) {
-                            out.append(",");
-                        }
-                        out.append(profileMap.get(vs));
-                    }
-                }
-                //We have a match so we stop looking
-                break;
-            }
-        }
-        if (out.length()==0)
+        if (concept.hasCoding("http://snomed.info/sct","709044004"))
         {
-            return null;
+            out = "CKD";
         }
-        return out.toString();
+        return out;
     }
 }
