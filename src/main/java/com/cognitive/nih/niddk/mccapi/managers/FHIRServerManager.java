10d28dbdabe26995a87f6e35bbe2b8d221980420
Jerry Goodnough <jerry.goodnough@stormwoods.com>
2020-11-06 11:59:24 -0800

    Updated to allow configuration at runtime

--- src/main/java/com/cognitive/nih/niddk/mccapi/managers/FHIRServerManager.java
+++ src/main/java/com/cognitive/nih/niddk/mccapi/managers/FHIRServerManager.java
@@ -1,20 +1,34 @@
 package com.cognitive.nih.niddk.mccapi.managers;
 
 import com.cognitive.nih.niddk.mccapi.data.FHIRServer;
+import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+@Log
+@Component
 public class FHIRServerManager {
+
+    @Value("${fhir.default.server.address:https://api.logicahealth.org/MCCeCarePlanTest/open}")
+    private String defaultFHIRServerAddress;
+    @Value("${fhir.secure.server.address:https://api.logicahealth.org/MCCeCarePlanTest/data}")
+    private String defaultFHIRSecureAddress;
+
     private FHIRServer defaultFHIRServer;
+
     private HashMap<String, FHIRServer> serverRegistry;
 
-    private static FHIRServerManager singleton = new FHIRServerManager();
+    private static FHIRServerManager singleton; // = new FHIRServerManager();
 
     public FHIRServerManager() {
         serverRegistry = new HashMap<>();
-        defineDefaultServers();
+        //defineDefaultServers();
     }
 
     public static FHIRServerManager getManager() {
@@ -30,15 +44,22 @@ public class FHIRServerManager {
         this.defaultFHIRServer = defaultFHIRServer;
     }
 
+    @PostConstruct
     private void defineDefaultServers() {
         FHIRServer srv = new FHIRServer();
-        srv.setBaseURL("https://api.logicahealth.org/MCCeCarePlanTest/open");
+        String env = System.getenv("DEFAULT_FHIR_SERVER");
+
+        //srv.setBaseURL("https://api.logicahealth.org/MCCeCarePlanTest/open");
+        srv.setBaseURL(defaultFHIRServerAddress);
         srv.setName("MMC eCarePlan Test");
         srv.setId("MCCeCarePlanTest");
         defaultFHIRServer = srv;
+        log.info("Default FHIR Server = "+defaultFHIRServerAddress);
         addServer(srv);
         srv = new FHIRServer();
-        srv.setBaseURL("https://api.logicahealth.org/MCCeCarePlanTest/data");
+        //srv.setBaseURL("https://api.logicahealth.org/MCCeCarePlanTest/data");
+        srv.setBaseURL(defaultFHIRSecureAddress);
+        log.info("Default FHIR Secure Server = "+defaultFHIRSecureAddress);
         srv.setName("MMC eCarePlan Test (Secure)");
         srv.setId("MCCeCarePlanTestSecure");
         addServer(srv);
