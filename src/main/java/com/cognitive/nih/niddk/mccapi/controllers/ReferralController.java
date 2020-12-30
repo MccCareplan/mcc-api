b5b8775218cbd1039279ae824824bc08938989c6
Jerry Goodnough <jerry.goodnough@stormwoods.com>
2020-12-29 20:04:02 -0800

    Preliminary Referral Support

--- src/main/java/com/cognitive/nih/niddk/mccapi/controllers/ReferralController.java
+++ src/main/java/com/cognitive/nih/niddk/mccapi/controllers/ReferralController.java
@@ -4,73 +4,51 @@ package com.cognitive.nih.niddk.mccapi.controllers;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
 import com.cognitive.nih.niddk.mccapi.data.Context;
 import com.cognitive.nih.niddk.mccapi.data.CounselingSummary;
-import com.cognitive.nih.niddk.mccapi.data.EducationSummary;
+import com.cognitive.nih.niddk.mccapi.data.ReferralSummary;
 import com.cognitive.nih.niddk.mccapi.managers.ContextManager;
 import com.cognitive.nih.niddk.mccapi.managers.QueryManager;
 import com.cognitive.nih.niddk.mccapi.mappers.CounselingMapper;
-import com.cognitive.nih.niddk.mccapi.mappers.EducationMapper;
+import com.cognitive.nih.niddk.mccapi.mappers.ReferralMapper;
 import com.cognitive.nih.niddk.mccapi.services.FHIRServices;
+import com.cognitive.nih.niddk.mccapi.util.Helper;
 import lombok.extern.slf4j.Slf4j;
 import org.hl7.fhir.r4.model.Bundle;
 import org.hl7.fhir.r4.model.Procedure;
+import org.hl7.fhir.r4.model.Reference;
 import org.hl7.fhir.r4.model.ServiceRequest;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.WebRequest;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Slf4j
 @RestController
 @CrossOrigin(origins = "*")
 
-public class CounselingController {
+public class ReferralController {
     private final QueryManager queryManager;
 
-    public CounselingController(QueryManager queryManager) {
+    public ReferralController(QueryManager queryManager) {
         this.queryManager = queryManager;
 
     }
 
-    @GetMapping("/summary/counselings")
-    public CounselingSummary[] getCounselingSummary(@RequestParam(required = true, name = "subject") String subjectId, @RequestParam(required = false, name = "careplan") String careplanId, @RequestHeader Map<String, String> headers, WebRequest webRequest) {
-        ArrayList<CounselingSummary> out = new ArrayList<>();
+    @GetMapping("/summary/referrals")
+    public ReferralSummary[] getCounselingSummary(@RequestParam(required = true, name = "subject") String subjectId, @RequestParam(required = false, name = "careplan") String careplanId, @RequestHeader Map<String, String> headers, WebRequest webRequest) {
+        ArrayList<ReferralSummary> out = new ArrayList<>();
 
         FHIRServices fhirSrv = FHIRServices.getFhirServices();
         IGenericClient client = fhirSrv.getClient(headers);
         Map<String,String> values = new HashMap<>();
 
-        String callUrl=queryManager.setupQuery("Counseling.Procedure.Query",values,webRequest);
+        String callUrl = queryManager.setupQuery("Referral.ServiceRequest.Query",values,webRequest);
 
 
         //Possible Sources
-        ///    Procedures - Education category = 409073007, Counseling 409063005
-        //     ServiceRequests - Same
-
-        if (callUrl != null) {
-            Bundle results = client.fetchResourceFromUrl(Bundle.class, callUrl);
-            // Bundle results = client.search().forResource(Goal.class).where(Goal.SUBJECT.hasId(subjectId))
-            //         .returnBundle(Bundle.class).execute();
-            Context ctx = ContextManager.getManager().findContextForSubject(subjectId, headers);
-            ctx.setClient(client);
-            for (Bundle.BundleEntryComponent e : results.getEntry()) {
-                if (e.getResource().fhirType().compareTo("Procedure")==0){
-                    Procedure p = (Procedure) e.getResource();
-                    //TODO: Add filter by status  active, completed
-                    //TODO: Filter by intent
-                    CounselingSummary cs = CounselingMapper.fhir2summary(p, ctx);
-                    out.add(cs);
-                }
-            }
-        }
-
-        callUrl=queryManager.setupQuery("Counseling.ServiceRequest.Query",values,webRequest);
-
-
-        //Possible Sources
-        ///    Procedures - Education category = 409073007, Counseling 409063005
-        //     ServiceRequests - Same
+        //     ServiceRequests - Education category = 409073007, Counseling 409063005
 
         if (callUrl != null) {
             Bundle results = client.fetchResourceFromUrl(Bundle.class, callUrl);
@@ -82,17 +60,48 @@ public class CounselingController {
                 if (e.getResource().fhirType().compareTo("ServiceRequest")==0){
                     ServiceRequest p = (ServiceRequest) e.getResource();
                     //TODO: Add filter by status  active, completed
-                    //TODO: Filter by intent
-                    CounselingSummary cs = CounselingMapper.fhir2summary(p, ctx);
-                    out.add(cs);
+
+                    if (p.hasRequester()) {
+                        //TODO: Filter by intent
+                        if (p.hasPerformer() ) {
+                            if (isPerformerProvider(p.getPerformer())) {
+                                ReferralSummary cs = ReferralMapper.fhir2summary(p, ctx);
+                                out.add(cs);
+                            }
+                        }
+                    }
                 }
             }
         }
 
-        CounselingSummary[] outA = new CounselingSummary[out.size()];
+        ReferralSummary[] outA = new ReferralSummary[out.size()];
         outA = out.toArray(outA);
         return outA;
     }
 
+    private boolean isPerformerProvider(List<Reference> performers)
+    {
+        boolean ok = true;
+
+        search:
+        for(Reference ref: performers)
+        {
+            String type = Helper.getReferenceType(ref);
+            switch(type)
+            {
+                case "Patient":
+                case "RelatedPerson":
+                {
+                    ok = false;
+                    break search;
+                }
+                default:
+                {
+                    break;
+                }
+            }
+        }
+        return ok;
+    }
 }
 
--- src/main/java/com/cognitive/nih/niddk/mccapi/data/Referral.java
+++ src/main/java/com/cognitive/nih/niddk/mccapi/data/Referral.java
@@ -1,20 +1,21 @@
 package com.cognitive.nih.niddk.mccapi.data;
 
+import com.cognitive.nih.niddk.mccapi.data.primative.GenericType;
 import com.cognitive.nih.niddk.mccapi.data.primative.MccCodeableConcept;
 import com.cognitive.nih.niddk.mccapi.data.primative.MccDate;
+import com.cognitive.nih.niddk.mccapi.data.primative.MccDateTime;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 @JsonInclude(JsonInclude.Include. NON_NULL)
 @Data
-public class Counseling {
+public class Referral {
 
-
-    private MccCodeableConcept topic;
-    private String type;
+    private MccCodeableConcept purpose;
     private String FHIRId;
-    private MccDate date;
-    private MccCodeableConcept outcome;
+    private GenericType date;
+    private String displayDate;
+    private String referrer;
+    private String receiver;
     private String status;
-
 }
