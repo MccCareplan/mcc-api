84184c15120854509127c5e3e613202e178fc6a6
Jerry Goodnough <jerry.goodnough@stormwoods.com>
2020-12-30 08:52:08 -0800

    Added Status Filtering for Procedures

--- src/main/java/com/cognitive/nih/niddk/mccapi/controllers/CounselingController.java
+++ src/main/java/com/cognitive/nih/niddk/mccapi/controllers/CounselingController.java
@@ -4,11 +4,9 @@ package com.cognitive.nih.niddk.mccapi.controllers;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
 import com.cognitive.nih.niddk.mccapi.data.Context;
 import com.cognitive.nih.niddk.mccapi.data.CounselingSummary;
-import com.cognitive.nih.niddk.mccapi.data.EducationSummary;
 import com.cognitive.nih.niddk.mccapi.managers.ContextManager;
 import com.cognitive.nih.niddk.mccapi.managers.QueryManager;
 import com.cognitive.nih.niddk.mccapi.mappers.CounselingMapper;
-import com.cognitive.nih.niddk.mccapi.mappers.EducationMapper;
 import com.cognitive.nih.niddk.mccapi.services.FHIRServices;
 import lombok.extern.slf4j.Slf4j;
 import org.hl7.fhir.r4.model.Bundle;
@@ -26,8 +24,35 @@ import java.util.Map;
 @CrossOrigin(origins = "*")
 
 public class CounselingController {
+    private static final int ACTIVE_LIST = 0;
+    private static final int INACTIVE_LIST = 1;
+    private static final int IGNORE = 2;
+    private static final HashMap<String, Integer> activeKeys = new HashMap<>();
+
+    static {
+        // Procedure Status
+        //		preparation | in-progress | not-done | on-hold | stopped | completed | entered-in-error | unknown
+        //
+
+        //Hash as verified
+        Integer active = Integer.valueOf(ACTIVE_LIST);
+        Integer inactive = Integer.valueOf(INACTIVE_LIST);
+        Integer ignore = Integer.valueOf(IGNORE);
+        //	active | recurrence | relapse
+        activeKeys.put("preparation", active);
+        activeKeys.put("in-progress", active);
+        activeKeys.put("not-done", active);
+        activeKeys.put("on-hold", active);
+        activeKeys.put("stopped", inactive);
+        activeKeys.put("completed", inactive);
+        activeKeys.put("entered-in-error", ignore);
+        activeKeys.put("unknown", ignore);
+
+    }
+
     private final QueryManager queryManager;
     private final boolean SERVICE_REQUEST_ENABLED = false;
+
 
     public CounselingController(QueryManager queryManager) {
         this.queryManager = queryManager;
@@ -40,9 +65,9 @@ public class CounselingController {
 
         FHIRServices fhirSrv = FHIRServices.getFhirServices();
         IGenericClient client = fhirSrv.getClient(headers);
-        Map<String,String> values = new HashMap<>();
+        Map<String, String> values = new HashMap<>();
 
-        String callUrl=queryManager.setupQuery("Counseling.Procedure.Query",values,webRequest);
+        String callUrl = queryManager.setupQuery("Counseling.Procedure.Query", values, webRequest);
 
 
         //Possible Sources
@@ -56,17 +81,22 @@ public class CounselingController {
             Context ctx = ContextManager.getManager().findContextForSubject(subjectId, headers);
             ctx.setClient(client);
             for (Bundle.BundleEntryComponent e : results.getEntry()) {
-                if (e.getResource().fhirType().compareTo("Procedure")==0){
+                if (e.getResource().fhirType().compareTo("Procedure") == 0) {
                     Procedure p = (Procedure) e.getResource();
-                    //TODO: Add filter by status  active, completed
-                    //TODO: Filter by intent
-                    CounselingSummary cs = CounselingMapper.fhir2summary(p, ctx);
-                    out.add(cs);
+                    if (p.hasStatus()) {
+                        String status = p.getStatus().toCode();
+                        Integer s = activeKeys.get(status);
+                        if (s != null && s.intValue() != IGNORE) {
+                            //TODO: Filter by intent
+                            CounselingSummary cs = CounselingMapper.fhir2summary(p, ctx);
+                            out.add(cs);
+                        }
+                    }
                 }
             }
         }
 
-        callUrl=queryManager.setupQuery("Counseling.ServiceRequest.Query",values,webRequest);
+        callUrl = queryManager.setupQuery("Counseling.ServiceRequest.Query", values, webRequest);
 
 
         //Possible Sources
@@ -80,7 +110,7 @@ public class CounselingController {
             Context ctx = ContextManager.getManager().findContextForSubject(subjectId, headers);
             ctx.setClient(client);
             for (Bundle.BundleEntryComponent e : results.getEntry()) {
-                if (e.getResource().fhirType().compareTo("ServiceRequest")==0){
+                if (e.getResource().fhirType().compareTo("ServiceRequest") == 0) {
                     ServiceRequest p = (ServiceRequest) e.getResource();
                     //TODO: Add filter by status  active, completed
                     //TODO: Filter by intent
