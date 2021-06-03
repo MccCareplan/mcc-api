366a19ae3a1dfa8212ebd38414ed2282f6b6f52c
Jerry Goodnough <jerry.goodnough@stormwoods.com>
2021-06-02 19:35:03 -0700

    Update to support the required headers.

---
+++ src/main/java/com/cognitive/nih/niddk/mccapi/exception/FHIRTokenHeaderMissingException.java
@@ -0,0 +1,9 @@
+package com.cognitive.nih.niddk.mccapi.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "Missing Header mcc-fhir-token")
+
+public class FHIRTokenHeaderMissingException extends RequiredHeaderException {
+}
--- src/main/java/com/cognitive/nih/niddk/mccapi/managers/ContextManager.java
+++ src/main/java/com/cognitive/nih/niddk/mccapi/managers/ContextManager.java
@@ -2,30 +2,63 @@ package com.cognitive.nih.niddk.mccapi.managers;
 
 import ca.uhn.fhir.rest.client.api.IGenericClient;
 import com.cognitive.nih.niddk.mccapi.data.Context;
+import com.cognitive.nih.niddk.mccapi.exception.FHIRServerHeaderMissingException;
+import com.cognitive.nih.niddk.mccapi.exception.FHIRTokenHeaderMissingException;
 import com.cognitive.nih.niddk.mccapi.mappers.IR4Mapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 
+import javax.annotation.PostConstruct;
 import java.util.HashMap;
 import java.util.Map;
 
 @Component
+@Slf4j
 public class ContextManager {
     //private static ContextManager singleton= new ContextManager();
     //public static ContextManager getManager() {return singleton;}
 
     //TODO: Replace with a cache
     HashMap<String, Context> subjectMap;
+    @Value("${mcc.request.header.require.server:false}")
+    private String headerRequireServer;
+    @Value("${mcc.request.header.require.token:false}")
+    private String headerRequireToken;
+    private boolean reqToken = false;
+    private boolean reqServer = false;
 
+    private static String headerServer = "mcc-fhir-server";
+    private static String headerToken = "mcc-fhir-token";
 
     public ContextManager(){
      subjectMap = new HashMap<>();
+    }
+
+    @PostConstruct
+    public void init()
+    {
+        reqToken = Boolean.parseBoolean(headerRequireToken);
+        log.info("Config: mcc.request.header.require.token = "+reqToken);
+        reqServer = Boolean.parseBoolean(headerRequireServer);
+        log.info("Config: mcc.request.header.require.server = "+reqServer);
     }
 
     public Context setupContext(String subjectId, IGenericClient client, IR4Mapper mapper, Map<String, String> headers)
     {
         Context out = null;
         Context fnd = null;
+
+        //Enfor
+        if (reqServer && !headers.containsKey(headerServer))
+        {
+            throw new FHIRServerHeaderMissingException();
+        }
+        if (reqToken && !headers.containsKey(headerToken))
+        {
+            throw new FHIRTokenHeaderMissingException();
+        }
 
         //Deal with calls with no subject - Which get a unique context
 
